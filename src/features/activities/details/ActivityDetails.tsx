642e389c3d9737c006e74780d93ad765161fdda9
j2.jeremias <j2.jeremias@gmail.com>
2024-10-28 02:11:56 +1000

    Initial commit

---
+++ src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,35 @@
+import { Grid } from "semantic-ui-react";
+import { useStore } from "../../../app/stores/store";
+import { observer } from "mobx-react-lite";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
+import ActivityDetailedHeader from "./ActivityDetailedHeader";
+import ActivityDetailedInfo from "./ActivityDetailedInfo";
+import ActivityDetailedChat from "./ActiviyDetailedChat";
+import ActivityDetailedSidebar from "./ActivityDetailedSidebar";
+
+export default observer(function ActivityDetails() {
+    const {activityStore} = useStore();
+    const {selectedActivity: activity, loadActivity, loadingInitial} = activityStore;
+    const {id} = useParams();
+
+    useEffect(() => {
+        if (id) loadActivity(id);
+    }, [id, loadActivity])
+
+    if (loadingInitial || !activity) return <LoadingComponent content={"Loading..."} />;
+    
+    return (
+        <Grid>
+            <Grid.Column width={10}>
+                <ActivityDetailedHeader activity={activity}/>
+                <ActivityDetailedInfo activity={activity} />
+                <ActivityDetailedChat />
+            </Grid.Column>
+            <Grid.Column width={6}>
+                <ActivityDetailedSidebar />
+            </Grid.Column>
+        </Grid>  
+    )
+})
---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import 'semantic-ui-css/semantic.min.css';
+import 'react-calendar/dist/Calendar.css';
+import 'react-toastify/dist/ReactToastify.min.css';
+import 'react-datepicker/dist/react-datepicker.css';
+import './app/layout/styles.css';
+import { store, StoreContext } from './app/stores/store';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './app/router/Routes';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <StoreContext.Provider value={store}>
+        <RouterProvider router={router}/>
+    </StoreContext.Provider>
+    
+  </StrictMode>,
+)
